ccc0819a45827df7d505a3d60ea594cb63445e0d
Shubham Raj <shubham14p3@gmail.com>
2020-06-30 23:17:30 +0530

    added console log for validation of working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import _ from 'lodash';
+import _ from "lodash";
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  console.log("Hello World");
 
   return element;
 }
