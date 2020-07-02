2ae22541fae79a3cb898c34ffb1fe436801336df
Shubham Raj <shubham14p3@gmail.com>
2020-07-02 23:25:30 +0530

    fix linters

--- src/index.js
+++ src/index.js
@@ -4,89 +4,107 @@ import About from './modules/about';
 import Contact from './modules/contact';
 
 const Restaurant = (function () {
-    const PAGE = {
-        HOME: 1,
-        MENU: 2,
-        ABOUT: 3,
-        CONTACT: 4
+  const PAGE = {
+    HOME: 1,
+    MENU: 2,
+    ABOUT: 3,
+    CONTACT: 4,
+  };
+  Home.loadNavigationBar();
+  Home.createCenterText();
+  let currentPage = PAGE.HOME;
+
+  const deleteNode = (node) => {
+    const containerDiv = document.getElementById(node);
+    // just deleting menuDiv won't help, you'll have to delete all its child nodes
+    while (containerDiv.firstElementChild) {
+      containerDiv.removeChild(containerDiv.lastElementChild);
     }
-    Home.loadNavigationBar();
-    Home.createCenterText();
-    let currentPage = PAGE.HOME;
+    containerDiv.remove();
+  };
 
-    const deleteNode = (node) => {
-        const containerDiv = document.getElementById(node);
-        //just deleting menuDiv won't help, you'll have to delete all its child nodes
-        while (containerDiv.firstElementChild) {
-            containerDiv.removeChild(containerDiv.lastElementChild);
-        }
-        containerDiv.remove();
+  const menuButton = document.getElementById('menuButton');
+  menuButton.addEventListener('click', () => {
+    switch (currentPage) {
+      case PAGE.HOME:
+        deleteNode('centerText');
+        break;
+      case PAGE.ABOUT:
+        deleteNode('aboutTextDiv');
+        break;
+      case PAGE.CONTACT:
+        deleteNode('contactInfo');
+        break;
+      default:
+        break;
     }
+    if (currentPage !== PAGE.MENU) {
+      currentPage = PAGE.MENU;
+      Menu.loadMenu();
+    }
+  });
 
-    const menuButton = document.getElementById("menuButton");
-    menuButton.addEventListener('click', () => {
+  const homeButton = document.getElementById('homeButton');
+  homeButton.addEventListener('click', () => {
+    switch (currentPage) {
+      case PAGE.MENU:
+        deleteNode('menu');
+        break;
+      case PAGE.ABOUT:
+        deleteNode('aboutTextDiv');
+        break;
+      case PAGE.CONTACT:
+        deleteNode('contactInfo');
+        break;
+      default:
+        break;
+    }
+    if (currentPage !== PAGE.HOME) {
+      currentPage = PAGE.HOME;
+      Home.createCenterText();
+    }
+  });
 
-        switch (currentPage) {
-            case PAGE.HOME: deleteNode("centerText");
-                break;
-            case PAGE.ABOUT: deleteNode("aboutTextDiv");
-                break;
-            case PAGE.CONTACT: deleteNode("contactInfo");
-                break;
-        }
-        if (currentPage !== PAGE.MENU) {
-            currentPage = PAGE.MENU;
-            Menu.loadMenu();
-        }
-    })
+  const aboutButton = document.getElementById('aboutButton');
+  aboutButton.addEventListener('click', () => {
+    switch (currentPage) {
+      case PAGE.MENU:
+        deleteNode('menu');
+        break;
+      case PAGE.HOME:
+        deleteNode('centerText');
+        break;
+      case PAGE.CONTACT:
+        deleteNode('contactInfo');
+        break;
+      default:
+        break;
+    }
+    if (currentPage !== PAGE.ABOUT) {
+      currentPage = PAGE.ABOUT;
+      About.createAbout();
+    }
+  });
 
-    const homeButton = document.getElementById("homeButton");
-    homeButton.addEventListener('click', () => {
-        switch (currentPage) {
-            case PAGE.MENU: deleteNode("menu");
-                break;
-            case PAGE.ABOUT: deleteNode("aboutTextDiv");
-                break;
-            case PAGE.CONTACT: deleteNode("contactInfo");
-                break;
-        }
-        if (currentPage !== PAGE.HOME) {
-            currentPage = PAGE.HOME;
-            Home.createCenterText();
-        }
-    });
-
-    const aboutButton = document.getElementById("aboutButton");
-    aboutButton.addEventListener('click', () => {
-        switch (currentPage) {
-            case PAGE.MENU: deleteNode("menu");
-                break;
-            case PAGE.HOME: deleteNode("centerText");
-                break;
-            case PAGE.CONTACT: deleteNode("contactInfo");
-                break;
-        }
-        if (currentPage !== PAGE.ABOUT) {
-            currentPage = PAGE.ABOUT;
-            About.createAbout();
-        }
-    });
-
-    const contactButton = document.getElementById("contactButton");
-    contactButton.addEventListener('click', () => {
-        switch (currentPage) {
-            case PAGE.MENU: deleteNode("menu");
-                break;
-            case PAGE.HOME: deleteNode("centerText");
-                break;
-            case PAGE.ABOUT: deleteNode("aboutTextDiv");
-                break;
-        }
-        if (currentPage !== PAGE.CONTACT) {
-            currentPage = PAGE.CONTACT;
-            Contact.createContact();
-        }
-    })
-})();
-
-
+  const contactButton = document.getElementById('contactButton');
+  contactButton.addEventListener('click', () => {
+    switch (currentPage) {
+      case PAGE.MENU:
+        deleteNode('menu');
+        break;
+      case PAGE.HOME:
+        deleteNode('centerText');
+        break;
+      case PAGE.ABOUT:
+        deleteNode('aboutTextDiv');
+        break;
+      default:
+        break;
+    }
+    if (currentPage !== PAGE.CONTACT) {
+      currentPage = PAGE.CONTACT;
+      Contact.createContact();
+    }
+  });
+}());
+export default Restaurant;
--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,69 +1,69 @@
 const Contact = (function () {
-    const contentDiv = document.getElementById("content");
-    const contactDiv = document.createElement("div");
-    contactDiv.setAttribute("id", "contactInfo");
+  const contentDiv = document.getElementById('content');
+  const contactDiv = document.createElement('div');
+  contactDiv.setAttribute('id', 'contactInfo');
 
-    const createHeading = (parentDiv, text) => {
-        const div = document.createElement("span");
-        div.innerHTML = text;
-        parentDiv.appendChild(div);
-    }
+  const createHeading = (parentDiv, text) => {
+    const div = document.createElement('span');
+    div.innerHTML = text;
+    parentDiv.appendChild(div);
+  };
 
-    const createInfo = (parentDiv, text) => {
-        const div = document.createElement("span");
-        div.innerHTML = text;
-        parentDiv.appendChild(div);
-    }
+  const createInfo = (parentDiv, text) => {
+    const div = document.createElement('span');
+    div.innerHTML = text;
+    parentDiv.appendChild(div);
+  };
 
-    const createTimingsDiv = () => {
-        const timingDiv = document.createElement("div");
-        contactDiv.appendChild(timingDiv);
-        timingDiv.setAttribute("id", "timingDiv");
-        timingDiv.innerHTML = '<i class="fa fa-3x fa-cutlery" aria-hidden="true"></i>';
-        
-        createHeading(timingDiv, "Timings");
-        createInfo(timingDiv, "9:00am - 12:00am");
-    }
+  const createTimingsDiv = () => {
+    const timingDiv = document.createElement('div');
+    contactDiv.appendChild(timingDiv);
+    timingDiv.setAttribute('id', 'timingDiv');
+    timingDiv.innerHTML = '<i class="fa fa-3x fa-cutlery" aria-hidden="true"></i>';
 
-    const createPhoneDiv = () => {
-        const phoneDiv = document.createElement("div");
-        contactDiv.appendChild(phoneDiv);
-        phoneDiv.setAttribute("id", "phoneDiv");
-        phoneDiv.innerHTML = '<i class="fa fa-3x fa-phone" aria-hidden="true"></i>';
+    createHeading(timingDiv, 'Timings');
+    createInfo(timingDiv, '9:00am - 12:00am');
+  };
 
-        createHeading(phoneDiv, "Phone");
-        createInfo(phoneDiv, "1234567899");
-    }
+  const createPhoneDiv = () => {
+    const phoneDiv = document.createElement('div');
+    contactDiv.appendChild(phoneDiv);
+    phoneDiv.setAttribute('id', 'phoneDiv');
+    phoneDiv.innerHTML = '<i class="fa fa-3x fa-phone" aria-hidden="true"></i>';
 
-    const createEmailDiv = () => {
-        const emailDiv = document.createElement("div");
-        contactDiv.appendChild(emailDiv);
-        emailDiv.setAttribute("id", "emailDiv");
-        emailDiv.innerHTML = '<i class="fa fa-3x fa-envelope-o" aria-hidden="true"></i>';
+    createHeading(phoneDiv, 'Phone');
+    createInfo(phoneDiv, '1234567899');
+  };
 
-        createHeading(emailDiv, "Email");
-        createInfo(emailDiv, "shubham14p3@gmail.com");
-    }
+  const createEmailDiv = () => {
+    const emailDiv = document.createElement('div');
+    contactDiv.appendChild(emailDiv);
+    emailDiv.setAttribute('id', 'emailDiv');
+    emailDiv.innerHTML = '<i class="fa fa-3x fa-envelope-o" aria-hidden="true"></i>';
 
-    const createLocationDiv = () => {
-        const locationDiv = document.createElement("div");
-        contactDiv.appendChild(locationDiv);
-        locationDiv.setAttribute("id", "locationDiv");
-        locationDiv.innerHTML = '<i class="fa fa-3x fa-map-marker" aria-hidden="true"></i>';
+    createHeading(emailDiv, 'Email');
+    createInfo(emailDiv, 'shubham14p3@gmail.com');
+  };
 
-        createHeading(locationDiv, "Location");
-        createInfo(locationDiv, "Jamshedpur, India");
-    }
+  const createLocationDiv = () => {
+    const locationDiv = document.createElement('div');
+    contactDiv.appendChild(locationDiv);
+    locationDiv.setAttribute('id', 'locationDiv');
+    locationDiv.innerHTML = '<i class="fa fa-3x fa-map-marker" aria-hidden="true"></i>';
 
-    const createContact = () => {
-        createTimingsDiv();
-        createPhoneDiv();
-        createEmailDiv();
-        createLocationDiv();
-        contentDiv.appendChild(contactDiv);
-    }
+    createHeading(locationDiv, 'Location');
+    createInfo(locationDiv, 'Jamshedpur, India');
+  };
 
-    return { createContact };
-})();
+  const createContact = () => {
+    createTimingsDiv();
+    createPhoneDiv();
+    createEmailDiv();
+    createLocationDiv();
+    contentDiv.appendChild(contactDiv);
+  };
+
+  return { createContact };
+}());
 
 export default Contact;
